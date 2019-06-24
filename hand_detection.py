4ecab156f094ad76d255a75bb803e41904394888
ashwinvaswani <ashwin.vaswani99@gmail.com>
2019-06-25 00:48:42 +0530

    Word prediction working

--- hand_detection.py
+++ hand_detection.py
@@ -15,51 +15,54 @@ import keras
 from keras.models import Sequential
 from keras import regularizers
 from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization
-from keras.applications import ResNet50,VGG16
+from keras.applications import ResNet50, VGG16
 from keras.applications.resnet50 import preprocess_input
-from keras import Model,layers
-from keras.models import load_model,model_from_json
+from keras import Model, layers
+from keras.models import load_model, model_from_json
 
 from utils import detector_utils as detector_utils
 
 labels_dict = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,'L':11,'M':12,
-                               'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,
-                               'Z':25,'nothing':26,'space':27,'delete':28,'complete':29}
+                   'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,
+                   'Z':25,'nothing':26,'space':27,'delete':28,'complete':29}
+
+
 
 def create_model():
-    
     model = Sequential()
-    
-    model.add(Conv2D(16, kernel_size = [3,3], padding = 'same', activation = 'relu', input_shape = (150,150,3)))
-    model.add(Conv2D(32, kernel_size = [3,3], padding = 'same', activation = 'relu'))
-    model.add(MaxPool2D(pool_size = [3,3]))
-    
-    model.add(Conv2D(32, kernel_size = [3,3], padding = 'same', activation = 'relu'))
-    model.add(Conv2D(64, kernel_size = [3,3], padding = 'same', activation = 'relu'))
-    model.add(MaxPool2D(pool_size = [3,3]))
-    
-    model.add(Conv2D(128, kernel_size = [3,3], padding = 'same', activation = 'relu'))
-    model.add(Conv2D(256, kernel_size = [3,3], padding = 'same', activation = 'relu'))
-    model.add(MaxPool2D(pool_size = [3,3]))
-    
+
+    model.add(Conv2D(16, kernel_size=[3, 3], padding='same', activation='relu', input_shape=(150, 150, 3)))
+    model.add(Conv2D(32, kernel_size=[3, 3], padding='same', activation='relu'))
+    model.add(MaxPool2D(pool_size=[3, 3]))
+
+    model.add(Conv2D(32, kernel_size=[3, 3], padding='same', activation='relu'))
+    model.add(Conv2D(64, kernel_size=[3, 3], padding='same', activation='relu'))
+    model.add(MaxPool2D(pool_size=[3, 3]))
+
+    model.add(Conv2D(128, kernel_size=[3, 3], padding='same', activation='relu'))
+    model.add(Conv2D(256, kernel_size=[3, 3], padding='same', activation='relu'))
+    model.add(MaxPool2D(pool_size=[3, 3]))
+
     model.add(BatchNormalization())
-    
+
     model.add(Flatten())
     model.add(Dropout(0.5))
-    model.add(Dense(512, activation = 'relu', kernel_regularizer = regularizers.l2(0.001)))
+    model.add(Dense(512, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
     model.add(Dropout(0.5))
-    model.add(Dense(26, activation = 'softmax'))
-    
-    model.compile(optimizer = 'adam', loss = keras.losses.categorical_crossentropy, metrics = ["accuracy"])
-    
+    model.add(Dense(26, activation='softmax'))
+
+    model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy, metrics=["accuracy"])
+
     print("MODEL CREATED")
     model.summary()
-    
+
     return model
 
+
 def fit_model():
-    model_hist = model.fit(X_train, Y_train, batch_size = 32, epochs = 20, validation_split = 0.1)
-    return model_hist 
+    model_hist = model.fit(X_train, Y_train, batch_size=32, epochs=20, validation_split=0.1)
+    return model_hist
+
 
 def get_labels_for_plot(predictions):
     predictions_labels = []
@@ -68,25 +71,27 @@ def get_labels_for_plot(predictions):
             predictions_labels.append(ins)
             return ins
 
-            #break
-    #return predictions_labels
+            # break
+    # return predictions_labels
+
 
 def load_test_dataa():
     images = []
     names = []
-    size = 50,50
+    size = 50, 50
     temp = cv2.imread('./img_thr2.jpg')
     temp = cv2.resize(temp, size)
-    #temp = cv2.cvtColor(temp,cv2.COLOR_RGB2GRAY)
+    # temp = cv2.cvtColor(temp,cv2.COLOR_RGB2GRAY)
     images.append(temp)
     names.append('img2')
     images = np.array(images)
-    images = images.astype('float32')/255.0
+    images = images.astype('float32') / 255.0
     return images, names
 
-#-------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------
 # Function - To find the running average over the background
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 def run_avg(image, aWeight):
     global bg
     # initialize the background
@@ -97,9 +102,10 @@ def run_avg(image, aWeight):
     # compute weighted average, accumulate it and update the background
     cv2.accumulateWeighted(image, bg, aWeight)
 
-#-------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------
 # Function - To segment the region of hand in the image
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 def segment(image, threshold=25):
     global bg
     # find the absolute difference between background and current frame
@@ -113,8 +119,8 @@ def segment(image, threshold=25):
 
     # get the contours in the thresholded image
     (cnts, _) = cv2.findContours(thresholded.copy(),
-                                    cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
+                                 cv2.RETR_EXTERNAL,
+                                 cv2.CHAIN_APPROX_SIMPLE)
 
     # return None, if no contours detected
     if len(cnts) == 0:
@@ -124,9 +130,50 @@ def segment(image, threshold=25):
         segmented = max(cnts, key=cv2.contourArea)
         return (thresholded, segmented)
 
+def recommend_word(text):
+        if len(text)==0:
+            return [None,None]
+        r = []
+        flag = 0
+
+        for i in words:
+            if i.startswith(text):
+                r.append(i)
+                if len(r) == 2:
+                    flag = 1
+                    return r
+
+        if flag == 0:
+            return [None,None]
+        
+
+
+
+
+def recommend_sentence(text):
+        if len(text)==0:
+            return [None,None]
+        sent = [text]
+        test = [func(sent[0])]
+        x1 = x.transform(test)
+        vals = cosine_similarity(x1, y)
+        vals = np.array(vals)
+        result = np.argsort(vals)
+        index1 = result[0][-1]
+        index2 = result[0][-2]
+        return ([data.loc[index1], data.loc[index2]])
+
+def func(text):
+        text = ' '.join([stem.stem(i) for i in text.split()])
+        text = ' '.join([lemma.lemmatize(i) for i in text.split()])
+        return text.lower()
+
+
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument('-d', '--display', dest='display', type=int,
-                        default=1, help='Display the detected images using OpenCV. This reduces FPS')
+                default=1, help='Display the detected images using OpenCV. This reduces FPS')
 args = vars(ap.parse_args())
 
 detection_graph, sess = detector_utils.load_inference_graph()
@@ -134,18 +181,52 @@ detection_graph, sess = detector_utils.load_inference_graph()
 if __name__ == '__main__':
     # Detection confidence threshold to draw bounding box
     score_thresh = 0.60
-    
-    #model = create_model()
-    #model.load_weights('Final_model_wn_weights.h5')
 
- 	####################################################
- 						#TODO (Add code here)
+    # model = create_model()
+    # model.load_weights('Final_model_wn_weights.h5')
 
- 	#Check if any .jpg files already exist or no. If yes, delete them
+    ####################################################
+    # TODO (Add code here)
 
- 	####################################################
+    import nltk,pandas as pd,numpy as np
+    data=pd.read_excel('Book1.xlsx')
+    from nltk.corpus import stopwords
 
-    model = load_model('new_model.h5')
+    nltk.download('stopwords')
+    stop = stopwords.words('english')
+    b=['where','how','what','who','why']
+    stop=list(set(stop)-set(b))
+
+    from nltk.stem import PorterStemmer, RegexpStemmer
+    stem = PorterStemmer()
+    nltk.download('wordnet')
+    from nltk import WordNetLemmatizer
+    lemma = WordNetLemmatizer()
+
+
+    data1=data
+    data1['data'] = data['data'].apply(func)
+
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.metrics.pairwise import cosine_similarity
+
+    x = TfidfVectorizer(stop_words=stop)
+    y = x.fit_transform(data1['data'])
+
+    from nltk import FreqDist
+    nltk.download('brown')
+    from nltk.corpus import brown
+    a = FreqDist(i.lower() for i in brown.words())
+    b = a.most_common()
+    words= []
+    for j in b:
+        if j[0] not in stop and len(j[0]) > 4:
+            words.append(j[0])
+
+
+    ####################################################
+
+    model = load_model('new_model2.h5')
     # Get stream from webcam and set parameters)
     vs = VideoStream().start()
 
@@ -161,9 +242,11 @@ if __name__ == '__main__':
     arr = []
     final_arr = []
     Sent_arr = []
-    recommended = "None"	
+    r1 = "None"
+    r2 = "None"
+    recommended = "None"
     counter = 0
-    #print("Enter first gesture")
+    # print("Enter first gesture")
     gesture_no = 1
 
     bg = None
@@ -174,9 +257,9 @@ if __name__ == '__main__':
         while True:
             # Read Frame and process
             frame = vs.read()
-            #frame = cv2.resize(frame, (320, 240))
+            # frame = cv2.resize(frame, (320, 240))
 
-            frame = cv2.flip(frame,1)
+            frame = cv2.flip(frame, 1)
 
             clone = frame.copy()
 
@@ -185,132 +268,133 @@ if __name__ == '__main__':
 
             # Convert image to rgb since opencv loads images in bgr, if not accuracy will decrease
             try:
-            	cv2.imwrite('frame_copy.jpg',frame)
-            	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imwrite('frame_copy.jpg', frame)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             except:
                 print("Error converting to RGB")
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (7, 7), 0)    
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
             if num_frames < 30:
                 run_avg(gray, aWeight)
             else:
-            # segment the hand region
+                # segment the hand region
                 hand = segment(gray)
 
-            # check whether hand region is segmented
+                # check whether hand region is segmented
                 if hand is not None:
                     # if yes, unpack the thresholded image and
                     # segmented region
                     (thresholded, segmented) = hand
-                    cv2.imwrite('threshold.jpg',thresholded)
+                    cv2.imwrite('threshold.jpg', thresholded)
 
                     # draw the segmented region and display the frame
-                    #cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
-                    #cv2.imshow("Thesholded", thresholded)
+                    # cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                    cv2.imshow("Thesholded", thresholded)
                     if cv2.waitKey(25) & 0xFF == ord('q'):
                         cv2.destroyAllWindows()
                         vs.stop()
                         break
 
-
             num_frames += 1
 
-            font=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX 
-            kernel=np.ones((3,3))
+            font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+            kernel = np.ones((3, 3))
 
-        
             # Run image through tensorflow graph
             boxes, scores, classes = detector_utils.detect_objects(
                 frame, detection_graph, sess)
-
 
             # Draw bounding boxeses and text
             detector_utils.draw_box_on_image(
                 num_hands_detect, score_thresh, scores, boxes, classes, im_width, im_height, frame)
 
             # When no hand is detected
-            if(max(scores) < 0.5):
-            	frame_copy = cv2.imread('frame_copy.jpg')
-            	cv2.imwrite("img_thr2.jpg",frame_copy)
+            if (max(scores) < 0.5):
+                frame_copy = cv2.imread('frame_copy.jpg')
+                cv2.imwrite("img_thr2.jpg", frame_copy)
 
-
-            #Preparing cropped image for prediction
-            tester_img,tester_name = load_test_dataa()
+            # Preparing cropped image for prediction
+            tester_img, tester_name = load_test_dataa()
             pred = model.predict_classes(tester_img)
             predictions_labels_plot = get_labels_for_plot(pred)
 
-            if(max(scores) < 0.5):
-            	predictions_labels_plot = "nothing"
+            if (max(scores) < 0.5):
+                predictions_labels_plot = "nothing"
 
-            if(len(arr) > 15 ):
+            if (len(arr) > 20):
                 for elmts in arr:
                     if predictions_labels_plot == elmts:
                         counter += 1
                         if counter == len(arr):
                             arr = []
                             counter = 0
-                            if(predictions_labels_plot == 'nothing'):
-                            
-                                if(len(final_arr) != 0):
-                                    #print("final_arr is :")
-                                    #print(''.join(final_arr))
+                            if (predictions_labels_plot == 'nothing'):
+
+                                if (len(final_arr) != 0):
+
                                     gesture_no = 1
-                                    Sent_arr.append(str(''.join(final_arr)))
+                                    Sent_arr.append(str(' '.join(final_arr)))
                                     final_arr = []
-                            elif(predictions_labels_plot == 'delete'):
-                            
-                                if(len(final_arr) != 0):
-                                    #print("final_arr is :")
-                                    #print(''.join(final_arr))
+                            elif (predictions_labels_plot == 'delete'):
+
+                                if (len(final_arr) != 0):
                                     final_arr.pop()
                                     gesture_no += 1
-                            elif(predictions_labels_plot == 'complete'):
-                            	
-                                ######################################################
-                                			#TODO( Add code here )
+                                    #print(str(''.join(final_arr)).lower())
+                                    r1,r2=recommend_word(str(''.join(final_arr)).lower())
+                                else:
+                                    r1,r2=recommend_sentence(word2tdf)
 
-                                #Append recommended word to Sent_arr
-                                #Set final_arr to []
-
-                                ######################################################
 
                                 gesture_no = 1
+                            elif (predictions_labels_plot == 'complete'):
+
+                                if(r1 != None):
+                                    Sent_arr.append(r1)
+                                    final_arr = []
+                                    word2tdf=r1
+                                    gesture_no=1
+
+                            elif (predictions_labels_plot == 'r2'):
+                                if(r2 != None):
+                                    Sent_arr.append(r2)
+                                    final_arr=[]
+                                    word2tdf=r2
+                                    gesture_no=1
+    
                             else:
                                 final_arr.append(str(predictions_labels_plot))
                                 gesture_no += 1
-                   				#####################################################
-                   							#TODO( Add code here )
-
-                   				#Predict word based on word existing in str(''.join(final_arr)) currently and set it in recommended variable
-                   				#Preferably create a fucntion for recommending and call that fxn here with str(''.join(final_arr)) as argument.
-                   				# I have initialized the recommended variable to "None". Please make whatever changes necessary.
-
-                   				#####################################################
-
+                            
+                                if len(final_arr)!=0:
+                                    #print(str(''.join(final_arr)).lower())
+                                    r1,r2=recommend_word(str(''.join(final_arr)).lower())
+                                   
+                                else:
+                                    r1,r2=recommend_sentence(word2tdf)
 
                                 break
 
             arr.append(predictions_labels_plot)
 
-            cv2.putText(frame, 'Enter gesture ' + str(gesture_no) + ': '+ str(predictions_labels_plot),
-                                    (int(im_width*0.01),int(im_height*0.1)),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
+            cv2.putText(frame, 'Enter gesture ' + str(gesture_no) + ': ' + str(predictions_labels_plot),
+                        (int(im_width * 0.01), int(im_height * 0.1)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
+            cv2.putText(frame, 'Recommendations -  1: ' + str(r1) + ' 2: ' + str(r2),
+                        (int(im_width * 0.01), int(im_height * 0.7)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-            cv2.putText(frame, 'Recommendation : ' +  recommended,
-                        (int(im_width*0.01),int(im_height*0.7)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
+            cv2.putText(frame, 'Word : ' + str(''.join(final_arr)),
+                        (int(im_width * 0.01), int(im_height * 0.8)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-            cv2.putText(frame, 'Word : ' +  str(''.join(final_arr)),
-                        (int(im_width*0.01),int(im_height*0.8)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
-
-            cv2.putText(frame, 'Sentence : ' +  str(''.join(Sent_arr)),
-                        (int(im_width*0.01),int(im_height*0.9)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
+            cv2.putText(frame, 'Sentence : ' + str(' '.join(Sent_arr)),
+                        (int(im_width * 0.01), int(im_height * 0.9)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
             # Calculate Frames per second (FPS)
             num_frames += 1
@@ -320,7 +404,7 @@ if __name__ == '__main__':
 
             if args['display']:
                 # Display FPS on frame
-                #detector_utils.draw_text_on_image("FPS : " + str("{0:.2f}".format(fps)), frame)
+                # detector_utils.draw_text_on_image("FPS : " + str("{0:.2f}".format(fps)), frame)
                 cv2.imshow('Detection', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     cv2.destroyAllWindows()
